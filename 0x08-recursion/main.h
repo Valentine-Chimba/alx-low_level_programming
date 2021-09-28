21b0b5131b74980a1ad2344c189c311a4d2ab77d
Valentine <valentinemudamba@gmail.com>
2021-09-28 22:35:42 +0300

    updated the file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,8 +2,6 @@
 #define MAIN_H
 
 /**
- * File: holberton.h
- * Auth: Brennan D Baraban
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x07-recursion directory.
  */
